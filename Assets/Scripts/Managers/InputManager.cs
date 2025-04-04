3e09dd1cd335ae3a299ee9989f97ece11d439df0
cetinye <cetinye@hotmail.com>
2025-04-04 12:07:40 +0300

    Camera drag control added

--- Assets/Scripts/Managers/InputManager.cs
+++ Assets/Scripts/Managers/InputManager.cs
@@ -1,5 +1,6 @@
 using StrategyGameDemo.Interfaces;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 namespace StrategyGameDemo.Managers
 {
@@ -8,6 +9,11 @@ namespace StrategyGameDemo.Managers
 		[SerializeField] private GameManager gameManager;
 		private Camera mainCamera;
 		private ISelectBehaviour selectedUnit;
+		
+		[Header("Camera Drag Control")]
+		private bool isDragging = false;
+		private Vector3 initialCameraPos;
+		private Vector2 initialMousePos;
 
 		private void Start()
 		{
@@ -16,8 +22,14 @@ namespace StrategyGameDemo.Managers
 
 		void Update()
 		{
+			if (IsMouseOverUI()) return;
+			
 			if (Input.GetMouseButtonDown(0))
 			{
+				isDragging = true;
+				initialMousePos = Input.mousePosition;
+				initialCameraPos = mainCamera.transform.position;
+				
 				RaycastHit2D hitInfo = Physics2D.Raycast(mainCamera.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
 				if (hitInfo && hitInfo.collider.TryGetComponent(out ISelectBehaviour clickable))
 				{
@@ -25,12 +37,40 @@ namespace StrategyGameDemo.Managers
 					clickable.LeftClick();
 				}
 			}
+			
+			if (Input.GetMouseButtonUp(0))
+			{
+				isDragging = false;
+			}
 
 			if (Input.GetMouseButtonDown(1))
 			{
 				if (selectedUnit != null)
 					selectedUnit.RightClick(mainCamera.ScreenToWorldPoint(Input.mousePosition));
 			}
+
+			CameraDrag();
+		}
+
+		private void CameraDrag()
+		{
+			if (isDragging && mainCamera != null && !IsMouseOverUI())
+			{
+				Vector2 currentMousePos = Input.mousePosition;
+				Vector2 delta = currentMousePos - initialMousePos;
+
+				float scalingFactor = 2 * mainCamera.orthographicSize / Screen.height;
+				float worldDeltaX = delta.x * scalingFactor;
+				float worldDeltaY = delta.y * scalingFactor;
+
+				Vector3 worldDelta = new Vector3(worldDeltaX, worldDeltaY, 0);
+				mainCamera.transform.position = initialCameraPos - worldDelta;
+			}
+		}
+		
+		private bool IsMouseOverUI()
+		{
+			return EventSystem.current.IsPointerOverGameObject();
 		}
 	}
 }
